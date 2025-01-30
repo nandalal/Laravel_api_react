24535921465b45bcb47247eb520de6cc09c5355c
Nandalal <s.nandalal35@gmail.com>
2025-01-30 06:14:17 +0530

    Add files via upload

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return Post::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        return Post::create($request->all());
+    }
+
+    public function show($id)
+    {
+        return Post::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->update($request->all());
+        return $post;
+    }
+
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        return response()->noContent();
+    }
+}
